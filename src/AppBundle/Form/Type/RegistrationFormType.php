0aa454ac52d154d7be2279bf198a5f7b4c88c535
Cid Lopes <alannettto@gmail.com>
2016-12-03 18:22:18 -0300

    Register view updated

--- src/AppBundle/Form/Type/RegistrationFormType.php
+++ src/AppBundle/Form/Type/RegistrationFormType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use AppBundle\Controller\GroupController;
 
@@ -15,7 +17,17 @@ class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
+        $builder
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'Username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
+                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Password Confirmation'),
+                'invalid_message' => 'Password Mismatch',
+            ))
+        ;
     }
 
 
